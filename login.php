c99c02285ee3bd567ea18058a4cb702f9e3fc8e9
martreba <martreba@greeny.cs.tlu.ee>
2017-01-17 03:32:06 +0200

    confirming checkbox submission

--- login.php
+++ login.php
@@ -1,7 +1,18 @@
 <?php 
-	
+	// login j2tab eposti meelde, v2ljad kohustuslikud
+	//signup 4 v2lja(oma idee)-esimene kodutoo lisa 2 v2lja(nt kaal ja pikkus)
+	//andmete salvestamine ja n2itamine oma idee j2rgi
 	require("/home/martreba/config.php");
-	require("functions.php");
+	//require("functions.php");
+	
+	// kui on sisseloginud siis suunan data lehele
+	if (isset($_SESSION["userId"])) {
+		header("Location: data.php");
+		exit();
+	}
+	
+	//var_dump($_POST);
+	//var_dump(isset($_POST["signupEmail"]));
 	
 	//var_dump($_GET);
 	
@@ -10,8 +21,12 @@
 	//var_dump($_POST);
 	
 	//MUUTUJAD
-	$signupEmailError = "*";
-	$signupEmail = "";
+	$signupEmailError = "";
+	$signupPasswordError = "";
+	$loginEmailError = "";
+	$loginPasswordError = "";
+	//$checkboxError = "";
+	
 	
 	//kas keegi vajutas nuppu ja see on olemas
 	
@@ -24,15 +39,26 @@
 			// on tühi
 			$signupEmailError = "* Väli on kohustuslik!";
 			
-		} else {
+		//} else {
 			// email on olemas ja õige
-			$signupEmail = $_POST["signupEmail"];
+			//$signupEmail = $_POST["signupEmail"];
 			
 		}
 		
 	} 
 	
-	$signupPasswordError = "*";
+	if(isset($_POST["loginEmail"])) {
+		$loginEmail = $_POST["loginEmail"];
+	} else {
+		$loginEmail = '';
+	}
+	
+	if(isset($_POST["signupEmail"])) {
+		$signupEmail = $_POST["signupEmail"];
+	} else {
+		$signupEmail = '';
+	}
+	
 	
 	if (isset ($_POST["signupPassword"])) {
 		
@@ -53,6 +79,13 @@
 		}
 		
 	}
+	if (!isset ($_POST["gender"])) {
+			
+			//error
+		}else {
+			// annad väärtuse
+		}
+
 	
 	//vaikimisi väärtus
 	$gender = "";
@@ -66,10 +99,24 @@
 		
 	} 
 	
+	if (isset ($_POST["loginEmail"])){
+		
+		if (empty ($_POST["loginEmail"])) {
+			
+			$loginEmailError = "Väli on kohustuslik";
+		}
+
+	}
+		if (isset ($_POST["loginPassword"])){
+		
+		if (empty ($_POST["loginPassword"])) {
+			
+			$loginPasswordError = "Väli on kohustuslik";
+		}
+	}
 	
 	
-	
-	if ( $signupEmailError == "*" AND
+	if ( $signupEmailError == "*" &&
 		 $signupPasswordError == "*" &&
 		 isset($_POST["signupEmail"]) && 
 		 isset($_POST["signupPassword"]) 
@@ -79,8 +126,9 @@
 		echo "Salvestan...<br>";
 		echo "email ".$signupEmail."<br>";
 		echo "parool ".$_POST["signupPassword"]."<br>";
-		
+		$signupEmail = $_POST["signupEmail"];
 		$password = hash("sha512", $_POST["signupPassword"]);
+	
 		
 		echo $password."<br>";
 		
@@ -89,15 +137,39 @@
 		
 	}
 	
-	// kas kasutaja tahab sisse logida
-	if ( isset($_POST["loginEmail"])&&
-		 isset($_POST["loginPassword"])&&
-		 !empty($_POST["loginEmail"])&&
-		 !empty($_POST["loginPassword"])
-		)
+		
+		$notice = "";
+	//kas kasutaja tahab sisse logida
+	if ( isset($_POST["loginEmail"]) && 
+		 isset($_POST["loginPassword"]) && 
+		 !empty($_POST["loginEmail"]) &&
+		 !empty($_POST["loginPassword"]) 
+	) {
+		
+		$notice = login($_POST["loginEmail"], $_POST["loginPassword"]);
+		
+	}
 	
-
+	if (isset ($_POST["check1"])){
+		
+		echo "";
+	} else {		
+		
+		if (empty ($_POST["check1"])) {
+			
+			$checkboxError = "Pead olema 18 et siseneda!";
+		
+		}
+	}
+		
+	
 ?>
+		
+			
+
+
+					
+					
 <!DOCTYPE html>
 <html>
 	<head>
@@ -105,16 +177,18 @@
 	</head>
 	<body>
 
-		<h1>Logi sisse</h1>
 		
+		<p>Idee on teha midagi sarnast gym trackerile, sarnane on ta ryhmatooga, aga erineb ta ryhmatoost selle poolest, et harjutused ei ole ainult bodyweight.</p>
+		
+		<h1>Logi sisse</h1>
 		<form method="POST" >
 			
 			<label>E-post</label><br>
-			<input name="loginEmail" type="email">
+			<input name="loginEmail" type="email" value="<?=$loginEmail;?>"> <?php echo $loginEmailError; ?>
 			
 			<br><br>
 
-			<input name="loginPassword" placeholder="Parool" type="password">
+			<input name="loginPassword" placeholder="Parool" type="password"> <?php echo $loginPasswordError; ?>
 			
 			<br><br>
 			
@@ -124,14 +198,14 @@
 		
 		<h1>Loo kasutaja</h1>
 		
-		<form method="POST" >
+		<form method="POST">
 			
 			<label>E-post</label><br>
 			<input name="signupEmail" type="email" value="<?=$signupEmail;?>"> <?php echo $signupEmailError; ?>
 			
 			<br><br>
 
-			<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?>
+			<input name="signupPassword" placeholder="Parool" type="password" > <?php echo $signupPasswordError; ?>
 			
 			<br><br>
 					
@@ -151,16 +225,17 @@
 			<?php if ($gender == "other") { ?>
 				<input type="radio" name="gender" value="other" checked> other<br>
 			<?php } else { ?>
-				<input type="radio" name="gender" value="other" > other<br>
+				<input type="radio" name="gender" value="other" > other<br><br>
 			<?php } ?>
 			
+			Olen vahemalt 18 aastat vana.<input type="checkbox" name="check1"><br> <?php echo $checkboxError; ?> <br>
+			
 			<input type="submit" value="Loo kasutaja">
+			
+			
 		
 		</form>
-		<form action="age">
-			  <input type="checkbox" name="age" value="age"> Olen vahemalt 18 aastat vana.<br>
-			  <input type="submit" value="Submit">
-		</form>
+		
 
 	</body>
 </html>
